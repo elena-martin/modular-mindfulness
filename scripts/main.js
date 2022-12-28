07a0a2d79ccb58c7338ee438cdc66d9aa9ebfc00
elena-martin <79723837+elena-martin@users.noreply.github.com>
2022-12-27 21:18:25 -0500

    trying new code

--- scripts/main.js
+++ scripts/main.js
@@ -180,7 +180,21 @@ function showNotif(){
       }
     ],
   };
-  const pushNotif = new PushEvent('push', options);
-
-  pushNotif.data.text();
+  self.addEventListener('push', (event) => {
+  
+    const data = event.data?.json() ?? {};
+    const title = data.title || "Something Has Happened";
+    const message = data.message || "Here's something you might want to check out.";
+    const icon = "images/new-notification.png";
+  
+    const notification = new self.Notification(title, {
+      body: message,
+      tag: 'simple-push-demo-notification',
+      icon,
+    });
+  
+    notification.addEventListener('click', () => {
+      clients.openWindow('https://example.blog.com/2015/03/04/something-new.html');
+    });
+  });
 }
