0393337524780fc8654fee4730d7e9f20e9f996c
elena-martin <79723837+elena-martin@users.noreply.github.com>
2022-12-27 20:43:04 -0500

    body > title

--- sw.js
+++ sw.js
@@ -48,7 +48,7 @@ self.addEventListener('push', function(event) {
 
   const title = 'Modular Mindfulness';
   const options = {
-    body: 'You have a notification!',
+    title: 'You have a notification!',
     icon: 'https://cdn.icon-icons.com/icons2/2620/PNG/512/among_us_player_red_icon_156942.png',
     badge: 'https://cdn.icon-icons.com/icons2/2620/PNG/512/among_us_player_red_icon_156942.png',
     actions: [
